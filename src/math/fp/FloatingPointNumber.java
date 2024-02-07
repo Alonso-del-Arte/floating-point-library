527c6869c0e1de7114c8c96c3fef0fd535273e40
Alonso del Arte <alonso.delarte@gmail.com>
2024-02-06 20:13:45 -0500

    Passing getBytes( ) does not leak field reference test

--- src/math/fp/FloatingPointNumber.java
+++ src/math/fp/FloatingPointNumber.java
@@ -26,7 +26,10 @@ public abstract class FloatingPointNumber
     final byte[] componentBytes;
     
     public byte[] getBytes() {
-        return this.componentBytes;
+        int length = this.componentBytes.length;
+        byte[] copy = new byte[length];
+        System.arraycopy(this.componentBytes, 0, copy, 0, length);
+        return copy;
     }
     
     public abstract float to32BitPrimitive();
