88be3857b39fa5be00abf287130b9218cc016386
Alonso del Arte <alonso.delarte@gmail.com>
2024-02-26 01:08:24 -0500

    New test class; failing toString( ) test for negative zero

---
+++ test/math/fp/QuarterPrecisionNumberNGTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package math.fp;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ * Tests of the QuarterPrecisionNumber class. Since there are only 256 possible 
+ * values, it is possible for these tests to be much more exhaustive and 
+ * complete than is possible for greater precision floating point numbers.
+ * @author Alonso del Arte
+ */
+public class QuarterPrecisionNumberNGTest {
+    
+    @Test
+    public void testToStringNegativeZero() {
+        QuarterPrecisionNumber number 
+                = new QuarterPrecisionNumber(Byte.MIN_VALUE);
+        String expected = "-0.0";
+        String actual = number.toString();
+        assertEquals(actual, expected);
+    }
+    
+}
