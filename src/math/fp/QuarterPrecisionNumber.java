218ff73aa478825bdbcc02fe97779b9c4a27c7e8
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-03 20:06:14 -0400

    Passing positive NaN should not be considered normal test

--- src/math/fp/QuarterPrecisionNumber.java
+++ src/math/fp/QuarterPrecisionNumber.java
@@ -51,7 +51,6 @@ public class QuarterPrecisionNumber extends FloatingPointNumber {
     // TODO: Write tests for this
     @Override
     public boolean isNormal() {
-        if (this.heldByte > 120) return true;
         return (this.heldByte & 120) != 120;
     }
     
