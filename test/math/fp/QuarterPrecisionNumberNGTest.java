42b4c4de1677d7976c1b202284e7d47640a131d6
Alonso del Arte <alonso.delarte@gmail.com>
2024-06-03 00:19:13 -0400

    Failing toString( ) test for positive with exponent 4

--- test/math/fp/QuarterPrecisionNumberNGTest.java
+++ test/math/fp/QuarterPrecisionNumberNGTest.java
@@ -179,6 +179,20 @@ public class QuarterPrecisionNumberNGTest {
     }
     
     @Test
+    public void testToStringPositiveExponentFour() {
+        Fraction currFract = ONE.times(16);
+        Fraction addend = ONE.times(2);
+        for (byte b = 88; b < 96; b++) {
+            QuarterPrecisionNumber number = new QuarterPrecisionNumber(b);
+            String expected = Double.toString(currFract.numericApproximation());
+            String actual = number.toString();
+            String message = "Bit pattern " + Integer.toHexString(b);
+            assertEquals(actual, expected, message);
+            currFract = currFract.plus(addend);
+        }
+    }
+    
+    @Test
     public void testToStringPositiveNaN() {
         String expected = "NaN";
         for (byte b = 121; b > 0; b++) {
