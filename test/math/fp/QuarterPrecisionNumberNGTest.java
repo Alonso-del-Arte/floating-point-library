cfe79d930771d5fc7a036235d58bde103e7b853f
Alonso del Arte <alonso.delarte@gmail.com>
2024-06-13 22:06:57 -0400

    Failing test for to 32-bit primitive test with exponent −2

--- test/math/fp/QuarterPrecisionNumberNGTest.java
+++ test/math/fp/QuarterPrecisionNumberNGTest.java
@@ -867,6 +867,20 @@ public class QuarterPrecisionNumberNGTest {
     }
     
     @Test
+    public void testTo32BitPrimitivePositiveExponentNegativeTwo() {
+        Fraction currFract = ONE_QUARTER;
+        Fraction addend = ONE.divides(32);
+        for (byte b = 40; b < 48; b++) {
+            QuarterPrecisionNumber number = new QuarterPrecisionNumber(b);
+            float expected = (float) currFract.numericApproximation();
+            float actual = number.to32BitPrimitive();
+            String message = "Bit pattern " + Integer.toHexString(b);
+            assertEquals(actual, expected, message);
+            currFract = currFract.plus(addend);
+        }
+    }
+    
+    @Test
     public void testTo32BitPrimitivePositiveExponentNegativeOne() {
         Fraction currFract = ONE_HALF;
         Fraction addend = ONE.divides(16);
