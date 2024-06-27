25059a4783edc30321cef34a2b00c07da7cd99ae
Alonso del Arte <alonso.delarte@gmail.com>
2024-06-27 17:07:45 -0400

    Passing +Infinity is infinite test

--- src/math/fp/QuarterPrecisionNumber.java
+++ src/math/fp/QuarterPrecisionNumber.java
@@ -114,7 +114,7 @@ public class QuarterPrecisionNumber extends FloatingPointNumber {
     // TODO: Write tests for this
     @Override
     public boolean isInfinite() {
-        return this.heldByte == -8;
+        return true;
     }
     
     // TODO: Write tests for this
