fc5ba8ae11e0422d92a6b84ddd9cce8cd5f5c81e
Alonso del Arte <alonso.delarte@gmail.com>
2024-07-24 20:52:26 -0400

    New package

---
+++ src/viewers/BitsViewer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package viewers;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class BitsViewer {
+    
+    // TODO: Write test for this
+    private static final Color DEFAULT_SIGN_BIT_COLOR = Color.BLACK;
+    
+    // TODO: Write test for this
+    private static final Color DEFAULT_EXPONENT_BITS_COLOR = Color.BLACK;
+    
+    // TODO: Write test for this
+    private static final Color DEFAULT_MANTISSA_BITS_COLOR = Color.BLACK;
+    
+}
---
+++ test/viewers/BitPanelNGTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package viewers;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class BitPanelNGTest extends JFrame {
+    
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
