cc881a261e192d0e721ab1773eed4a62655dd5cf
Alonso del Arte <alonso.delarte@gmail.com>
2024-07-25 22:19:21 -0400

    Failing getStatus( ) for instance constructed with status bit off test

--- test/viewers/BitPanelNGTest.java
+++ test/viewers/BitPanelNGTest.java
@@ -51,6 +51,13 @@ public class BitPanelNGTest extends JFrame {
     }
     
     @Test
+    public void testGetStatusConstructedWithFalse() {
+        short index = (short) RANDOM.nextInt(64);
+        BitPanel instance = new BitPanel(false, chooseColor(), index);
+        assert !instance.getStatus() : "Instance should have status bit off";
+    }
+    
+    @Test
     public void testConstructorRejectsNegativeIndex() {
         short badIndex = (short) (RANDOM.nextInt() | Short.MIN_VALUE);
         String msg = "Should not have been able to create bit panel with index " 
