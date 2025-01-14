51cb29ec95d5301f6c97aed28978c21ca438adc7
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-14 01:35:57 -0500

    Failing toString( ) tests for normal number with exponent 0

--- test/math/fp/HalfPrecisionNumberNGTest.java
+++ test/math/fp/HalfPrecisionNumberNGTest.java
@@ -50,6 +50,23 @@ public class HalfPrecisionNumberNGTest {
     }
     
     @Test
+    public void testToStringNegativeNormalExponentZero() {
+        double curr = 1.0;
+        double augend = 1.0 / 1024;
+        short start = Short.MIN_VALUE + 15360;
+        short stop = (short) (start + 1024);
+        for (short sh = start; sh < stop; sh++) {
+            HalfPrecisionNumber instance = new HalfPrecisionNumber(sh);
+            String expected = MINUS_SIGN + Double.toString(curr);
+            String actual = instance.toString();
+            String message = "For bit pattern " 
+                    + Integer.toHexString(sh).substring(4);
+            assertEquals(actual, expected, message);
+            curr += augend;
+        }
+    }
+    
+    @Test
     public void testToStringNegativeNormalExponentNegative1() {
         double curr = 0.5;
         int twoToThe11th = 1 << 11;
@@ -614,6 +631,23 @@ public class HalfPrecisionNumberNGTest {
         int twoToThe11th = 1 << 11;
         double augend = 1.0 / twoToThe11th;
         short start = 14336;
+        short stop = (short) (start + 1024);
+        for (short sh = start; sh < stop; sh++) {
+            HalfPrecisionNumber instance = new HalfPrecisionNumber(sh);
+            String expected = Double.toString(curr);
+            String actual = instance.toString();
+            String message = "For bit pattern " 
+                    + Integer.toHexString(sh + 65536).substring(1);
+            assertEquals(actual, expected, message);
+            curr += augend;
+        }
+    }
+    
+    @Test
+    public void testToStringPositiveNormalExponentZero() {
+        double curr = 1.0;
+        double augend = 1.0 / 1024;
+        short start = 15360;
         short stop = (short) (start + 1024);
         for (short sh = start; sh < stop; sh++) {
             HalfPrecisionNumber instance = new HalfPrecisionNumber(sh);
