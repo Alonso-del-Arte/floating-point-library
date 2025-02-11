11199e13dcd9aafcbe926f7185a8bce0669ab71f
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-10 19:11:56 -0500

    Failing isNaN( ) test

--- test/math/fp/HalfPrecisionNumberNGTest.java
+++ test/math/fp/HalfPrecisionNumberNGTest.java
@@ -1327,17 +1327,25 @@ public class HalfPrecisionNumberNGTest {
     }
 
     /**
-     * Test of isNaN method, of class HalfPrecisionNumber.
+     * Test of the isNaN function, of the HalfPrecisionNumber class.
      */
-//    @Test
+    @Test
     public void testIsNaN() {
         System.out.println("isNaN");
-        HalfPrecisionNumber instance = null;
-        boolean expResult = false;
-        boolean result = instance.isNaN();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        for (short sh = 31745; sh > 0; sh++) {
+            short negSh = (short) (sh + Short.MIN_VALUE);
+            HalfPrecisionNumber negativeNaN 
+                    = new HalfPrecisionNumber(negSh);
+            HalfPrecisionNumber positiveNaN = new HalfPrecisionNumber(sh);
+            String negNaNMsg = "Number " + negativeNaN + " from bit pattern " 
+                    + Integer.toHexString(negSh).substring(4) 
+                    + " should be found to be NaN";
+            String posNaNMsg = "Number " + positiveNaN + " from bit pattern " 
+                    + Integer.toHexString(sh + 65536).substring(1) 
+                    + " should be found to be NaN";
+            assert negativeNaN.isNaN() : negNaNMsg;
+            assert positiveNaN.isNaN() : posNaNMsg;
+        }
     }
 
     /**
