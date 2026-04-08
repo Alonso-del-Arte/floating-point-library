41659aeba1aa3dc471632a19bf1082f55ed3b710
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-07 23:52:04 -0400

    Passing not equals diff value test

--- src/math/fp/HalfPrecisionNumber.java
+++ src/math/fp/HalfPrecisionNumber.java
@@ -298,7 +298,7 @@ public class HalfPrecisionNumber extends FloatingPointNumber {
     // TODO: Write tests for this
     @Override
     public boolean equals(Object obj) {
-        return obj instanceof HalfPrecisionNumber;
+        return this == obj;
     }
     
     // TODO: Write tests for this
