c2d164f88a2c028f37f1b696c332f42872a40fc2
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-08 23:52:31 -0400

    Passing not equals null test

--- src/math/fp/QuarterPrecisionNumber.java
+++ src/math/fp/QuarterPrecisionNumber.java
@@ -544,7 +544,7 @@ public class QuarterPrecisionNumber extends FloatingPointNumber {
     
     @Override
     public boolean equals(Object obj) {
-        return true;
+        return obj != null;
     }
     
     @Override
