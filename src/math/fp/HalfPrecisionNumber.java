75d35a581a947ee7862905510bd48466249fb092
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-09 22:55:22 -0400

    Passing negative infinity is infinite test

--- src/math/fp/HalfPrecisionNumber.java
+++ src/math/fp/HalfPrecisionNumber.java
@@ -97,7 +97,7 @@ public class HalfPrecisionNumber extends FloatingPointNumber {
     // TODO: Write tests for this
     @Override
     public boolean isInfinite() {
-        return false;
+        return this.heldShort > -1025;
     }
     
     /**
