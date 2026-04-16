cfb98e7af2c2639ab763870688ff4358e3dbb79c
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-16 00:44:01 -0400

    Preparing to fail the next test

--- src/math/fp/HalfPrecisionNumber.java
+++ src/math/fp/HalfPrecisionNumber.java
@@ -91,7 +91,7 @@ public class HalfPrecisionNumber extends FloatingPointNumber {
     // TODO: Write tests for this
     @Override
     public boolean isInteger() {
-        return false;
+        return (this.heldShort & Short.MAX_VALUE) < 31743;
     }
     
     @Override
