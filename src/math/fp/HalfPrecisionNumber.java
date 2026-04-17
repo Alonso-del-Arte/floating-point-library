9e214d7c4f4539cd4d3acde0158e357498f0d70e
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-16 23:51:28 -0400

    Decided to test actual integer next for isInteger( )

--- src/math/fp/HalfPrecisionNumber.java
+++ src/math/fp/HalfPrecisionNumber.java
@@ -91,7 +91,7 @@ public class HalfPrecisionNumber extends FloatingPointNumber {
     // TODO: Write tests for this
     @Override
     public boolean isInteger() {
-        return (this.heldShort & Short.MAX_VALUE) < 31743;
+        return false;// (this.heldShort & Short.MAX_VALUE) < 31743;
     }
     
     @Override
