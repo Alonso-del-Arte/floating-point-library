0d6259b375d8be65b33e4ddfd4a4f202c5ea894d
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-21 23:31:10 -0400

    New test class

---
+++ test/math/fp/SinglePrecisionNumberNGTest.java
@@ -0,0 +1,414 @@
+/*
+ * Copyright (C) 2026 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package math.fp;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author al
+ */
+public class SinglePrecisionNumberNGTest {
+    
+    public SinglePrecisionNumberNGTest() {
+    }
+
+    /**
+     * Test of getUnbiasedExponent method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testGetUnbiasedExponent() {
+        System.out.println("getUnbiasedExponent");
+        SinglePrecisionNumber instance = null;
+        int expResult = 0;
+        int result = instance.getUnbiasedExponent();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getBiasedExponent method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testGetBiasedExponent() {
+        System.out.println("getBiasedExponent");
+        SinglePrecisionNumber instance = null;
+        int expResult = 0;
+        int result = instance.getBiasedExponent();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isNormal method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testIsNormal() {
+        System.out.println("isNormal");
+        SinglePrecisionNumber instance = null;
+        boolean expResult = false;
+        boolean result = instance.isNormal();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isSubnormal method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testIsSubnormal() {
+        System.out.println("isSubnormal");
+        SinglePrecisionNumber instance = null;
+        boolean expResult = false;
+        boolean result = instance.isSubnormal();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isInteger method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testIsInteger() {
+        System.out.println("isInteger");
+        SinglePrecisionNumber instance = null;
+        boolean expResult = false;
+        boolean result = instance.isInteger();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isFinite method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testIsFinite() {
+        System.out.println("isFinite");
+        SinglePrecisionNumber instance = null;
+        boolean expResult = false;
+        boolean result = instance.isFinite();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isInfinite method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testIsInfinite() {
+        System.out.println("isInfinite");
+        SinglePrecisionNumber instance = null;
+        boolean expResult = false;
+        boolean result = instance.isInfinite();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isNaN method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testIsNaN() {
+        System.out.println("isNaN");
+        SinglePrecisionNumber instance = null;
+        boolean expResult = false;
+        boolean result = instance.isNaN();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isQuietNaN method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testIsQuietNaN() {
+        System.out.println("isQuietNaN");
+        SinglePrecisionNumber instance = null;
+        boolean expResult = false;
+        boolean result = instance.isQuietNaN();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isSignalingNaN method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testIsSignalingNaN() {
+        System.out.println("isSignalingNaN");
+        SinglePrecisionNumber instance = null;
+        boolean expResult = false;
+        boolean result = instance.isSignalingNaN();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of to32BitPrimitive method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testTo32BitPrimitive() {
+        System.out.println("to32BitPrimitive");
+        SinglePrecisionNumber instance = null;
+        float expResult = 0.0F;
+        float result = instance.to32BitPrimitive();
+        assertEquals(result, expResult, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of to64BitPrimitive method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testTo64BitPrimitive() {
+        System.out.println("to64BitPrimitive");
+        SinglePrecisionNumber instance = null;
+        double expResult = 0.0;
+        double result = instance.to64BitPrimitive();
+        assertEquals(result, expResult, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toQuarterPrecision method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testToQuarterPrecision() {
+        System.out.println("toQuarterPrecision");
+        SinglePrecisionNumber instance = null;
+        QuarterPrecisionNumber expResult = null;
+        QuarterPrecisionNumber result = instance.toQuarterPrecision();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toHalfPrecision method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testToHalfPrecision() {
+        System.out.println("toHalfPrecision");
+        SinglePrecisionNumber instance = null;
+        HalfPrecisionNumber expResult = null;
+        HalfPrecisionNumber result = instance.toHalfPrecision();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toSinglePrecision method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testToSinglePrecision() {
+        System.out.println("toSinglePrecision");
+        SinglePrecisionNumber instance = null;
+        SinglePrecisionNumber expResult = null;
+        SinglePrecisionNumber result = instance.toSinglePrecision();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toDoublePrecision method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testToDoublePrecision() {
+        System.out.println("toDoublePrecision");
+        SinglePrecisionNumber instance = null;
+        DoublePrecisionNumber expResult = null;
+        DoublePrecisionNumber result = instance.toDoublePrecision();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toQuadruplePrecision method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testToQuadruplePrecision() {
+        System.out.println("toQuadruplePrecision");
+        SinglePrecisionNumber instance = null;
+        QuadruplePrecisionNumber expResult = null;
+        QuadruplePrecisionNumber result = instance.toQuadruplePrecision();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toOctuplePrecision method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testToOctuplePrecision() {
+        System.out.println("toOctuplePrecision");
+        SinglePrecisionNumber instance = null;
+        OctuplePrecisionNumber expResult = null;
+        OctuplePrecisionNumber result = instance.toOctuplePrecision();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of plus method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testPlus() {
+        System.out.println("plus");
+        FloatingPointNumber addend = null;
+        SinglePrecisionNumber instance = null;
+        FloatingPointNumber expResult = null;
+        FloatingPointNumber result = instance.plus(addend);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of negate method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testNegate() {
+        System.out.println("negate");
+        SinglePrecisionNumber instance = null;
+        FloatingPointNumber expResult = null;
+        FloatingPointNumber result = instance.negate();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of minus method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testMinus() {
+        System.out.println("minus");
+        FloatingPointNumber subtrahend = null;
+        SinglePrecisionNumber instance = null;
+        FloatingPointNumber expResult = null;
+        FloatingPointNumber result = instance.minus(subtrahend);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of times method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testTimes() {
+        System.out.println("times");
+        FloatingPointNumber multiplicand = null;
+        SinglePrecisionNumber instance = null;
+        FloatingPointNumber expResult = null;
+        FloatingPointNumber result = instance.times(multiplicand);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of reciprocal method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testReciprocal() {
+        System.out.println("reciprocal");
+        SinglePrecisionNumber instance = null;
+        FloatingPointNumber expResult = null;
+        FloatingPointNumber result = instance.reciprocal();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of divides method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testDivides() {
+        System.out.println("divides");
+        FloatingPointNumber divisor = null;
+        SinglePrecisionNumber instance = null;
+        FloatingPointNumber expResult = null;
+        FloatingPointNumber result = instance.divides(divisor);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of arithmeticallyEqual method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testArithmeticallyEqual() {
+        System.out.println("arithmeticallyEqual");
+        FloatingPointNumber other = null;
+        SinglePrecisionNumber instance = null;
+        boolean expResult = false;
+        boolean result = instance.arithmeticallyEqual(other);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of bitPatternHexadecimal method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testBitPatternHexadecimal() {
+        System.out.println("bitPatternHexadecimal");
+        SinglePrecisionNumber instance = null;
+        String expResult = "";
+        String result = instance.bitPatternHexadecimal();
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of fromPrimitive method, of class SinglePrecisionNumber.
+     */
+    @Test
+    public void testFromPrimitive() {
+        System.out.println("fromPrimitive");
+        float number = 0.0F;
+        SinglePrecisionNumber expResult = null;
+        SinglePrecisionNumber result = SinglePrecisionNumber.fromPrimitive(number);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
