ac93ddf1d3d7d3a008ae74ecff324acde9e1ac7f
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-31 23:58:40 -0400

    Failing null array test for fromBytes( )

--- test/math/integer/IntProcessorNGTest.java
+++ test/math/integer/IntProcessorNGTest.java
@@ -32,6 +32,19 @@ import org.testng.annotations.Test;
 public class IntProcessorNGTest {
     
     @Test
+    public void testFromBytesRejectsNullByteArray() {
+        String msg = "Using null array of should cause NPE";
+        Throwable t = assertThrows(() -> {
+            int badInt = IntProcessor.fromBytes(null);
+            System.out.println(msg + ", not given result " + badInt);
+        }, IllegalArgumentException.class, msg);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+
+    @Test
     public void testFromZeroBytesGivesZero() {
         byte[] source = {};
         int expected = 0;
